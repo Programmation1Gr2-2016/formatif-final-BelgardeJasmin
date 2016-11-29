185fc825251cb3b699d638625be4e8564e0a631b
BelgardeJasmin <belgarde.jasmin@carrefour.cegepvicto.ca>
2016-11-29 13:33:31 -0500

    ExamenFini

--- ExamenFormatif3/Question5/Program.cs
+++ ExamenFormatif3/Question5/Program.cs
@@ -22,11 +22,12 @@ namespace Question5
             bool toucheVF = false;
             string touches;
             bool tabFini = false;
+            int Tentative = 0;
 
 
 
 
-
+            joueur = ranTruFal.Next(0, 6);
             Niveau­[0] = true;
             Niveau[99] = true;
             while (tabFini != true)
@@ -45,13 +46,14 @@ namespace Question5
 
                         if (nbFalse == 4)
                         {
-                            break;
+                            Niveau[i] = true;
+                            nbFalse = 0;
 
                         }
 
                     }
 
-                    if (i == Niveau.Length - 1)
+                    if (i == Niveau.Length - 2)
                     {
                         tabFini = true;
                     }
@@ -59,11 +61,15 @@ namespace Question5
             }
             do
             {
+                toucheVF = false;
+                Tentative++;
+                Console.WriteLine();
                 Console.WriteLine("A déplace de 3 cases vers la gauche");
                 Console.WriteLine("S déplace de 2 cases vers la gauche ");
                 Console.WriteLine("D déplace de 1 case vers la gauche ");
                 Console.WriteLine("G déplace de 2 cases vers la droite ");
                 Console.WriteLine("H déplace de 4 cases vers la droite");
+                Console.WriteLine("Tentative numéro : " + Tentative);
                 Console.WriteLine("Joueur à la position " + joueur);
                 do
                 {
@@ -75,9 +81,10 @@ namespace Question5
                     {
 
                         case "A":
-
-                            if ((joueur - 3) < 0 && Niveau[joueur - 3] == true)
+                            toucheVF = true;
+                            if ((joueur - 3) > 0 && Niveau[joueur - 3] == true)
                             {
+
                                 joueur -= 3;
                             }
                             else
@@ -86,9 +93,10 @@ namespace Question5
                             }
                             break;
                         case "S":
-
-                            if ((joueur - 2) < 0 && Niveau[joueur - 2] == true)
+                            toucheVF = true;
+                            if ((joueur - 2) > 0 && Niveau[joueur - 2] == true)
                             {
+
                                 joueur -= 2;
                             }
                             else
@@ -97,9 +105,10 @@ namespace Question5
                             }
                             break;
                         case "D":
-
-                            if ((joueur - 1) < 0 && Niveau[joueur - 1] == true)
+                            toucheVF = true;
+                            if ((joueur - 1) > 0 && Niveau[joueur - 1] == true)
                             {
+
                                 joueur -= 1;
                             }
                             else
@@ -108,20 +117,23 @@ namespace Question5
                             }
                             break;
                         case "G":
-
-                            if ((joueur + 2) > 99 && Niveau[joueur + 2] == true)
+                            toucheVF = true;
+                            if ((joueur + 2) < 99 && Niveau[joueur + 2] == true)
                             {
+
                                 joueur += 2;
                             }
                             else
                             {
+
                                 Console.WriteLine("Déplacement impossible");
                             }
                             break;
                         case "H":
-
-                            if ((joueur + 4) > 99 && Niveau[joueur + 4] == true)
+                            toucheVF = true;
+                            if ((joueur + 4) < 99 && Niveau[joueur + 4] == true)
                             {
+
                                 joueur += 4;
                             }
                             else
@@ -131,6 +143,14 @@ namespace Question5
                             break;
                         case "Y":
                             AffichageEntier(Niveau, joueur);
+                            toucheVF = true;
+                            break;
+                        case "P":
+                            Affichage10(Niveau, joueur);
+                            toucheVF = true;
+                            break;
+                        case "Q":
+                            toucheVF = true;
                             break;
                         default:
                             Console.WriteLine("Entrer invalide");
@@ -166,9 +186,26 @@ namespace Question5
                 Console.Write(".");
             }
         }
-        private static void Affichage10()
+        private static void Affichage10(bool[] niveau, int joueur)
         {
 
+            for (int i = joueur; i < joueur + 10; i++)
+            {
+                if (i == joueur)
+                {
+                    Console.Write("*");
+                }
+                else if (niveau[i] == true)
+                {
+                    Console.Write("_");
+                }
+                else
+                {
+                    Console.Write("|");
+                }
+
+                Console.Write(".");
+            }
         }
     }
 }
